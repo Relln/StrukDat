0403a469faed26d85e328096c2bfef590b0b1c1f
Relln <farrel.novrizha@gmail.com>
2026-04-30 19:58:19 +0700

    Percobaan 2

--- Praktikum09/StackTugasMahasiswa10.java
+++ Praktikum09/StackTugasMahasiswa10.java
@@ -74,5 +74,22 @@ public class StackTugasMahasiswa10 {
         }
         System.out.println(" ");
     }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi10 stack = new StackKonversi10();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
+
+
+
 }
 

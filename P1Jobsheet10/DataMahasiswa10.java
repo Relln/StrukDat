cd1e79a94063e007f5cb4042660bccf13ceed708
Relln <farrel.novrizha@gmail.com>
2026-05-10 19:30:55 +0700

    Tugas Praktikum

---
+++ P1Jobsheet10/DataMahasiswa10.java
@@ -0,0 +1,17 @@
+package P1Jobsheet10;
+
+public class DataMahasiswa10 {
+    
+    String nama, nim;
+
+    public DataMahasiswa10(String nim, String nama) {
+        this.nama = nama;
+        this.nim = nim;
+    }
+
+    public String toString() {
+        return "NIM: " + nim + ", Nama: " + nama;
+    }
+}
+
+
